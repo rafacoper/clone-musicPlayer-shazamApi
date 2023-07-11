b38c17fcc7b6e5b7e772924c06f793fc926aff95
rafa-opb <rafaelbarbosa.tecnologia@outlook.com>
2023-07-11 15:02:08 -0300

    App builded with the JS Mastery

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import playerReducer from './features/playerSlice';
+import { shazamCoreApi } from './services/shazamCoreApi';
 
 export const store = configureStore({
   reducer: {
+    [shazamCoreApi.reducerPath]: shazamCoreApi.reducerPath.reducer,
     player: playerReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(shazamCoreApi.middleware),
 });
